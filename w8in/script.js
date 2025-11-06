380a28f3afce585a3aa47f5d53d69f858d3481f8
MidS1gn@67 <phualt67@gmail.com>
2025-11-05 21:31:22 -0600

    Delete w8in directory

--- w8in/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const tiles = document.querySelectorAll('.tile');
-
-    tiles.forEach(tile => {
-        tile.addEventListener('click', () => {
-            alert(`You clicked the ${tile.classList[1]} tile!`);
-        });
-    });
-});
